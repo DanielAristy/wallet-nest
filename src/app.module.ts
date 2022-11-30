73d36e5218389208a7b2091271d66866e5e93949
Daniel Aristizabal <daniel.aristizabal@sofka.com.co>
2022-11-30 15:24:06 -0500

    Se realiza conexion a base de datos con postgresql y se generan el modelo de entidades de base de datos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+import { TokenEntity } from './entities/TokenEntity';
+import { MovementEntity } from './entities/MovementEntity';
+import { ClientEntity } from './entities/ClientEntity';
+import { AppEntity } from './entities/AppEntity';
+import { AccountEntity } from './entities/AccountEntity';
 import { AppService } from './app.service';
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'admin',
+      password: 'admin',
+      database: 'postgres',
+      entities: [AccountEntity, AppEntity, ClientEntity, MovementEntity, TokenEntity],
+      synchronize: false,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/entities/ClientEntity.ts
@@ -0,0 +1,55 @@
+import { TokenEntity } from './TokenEntity';
+import { AccountEntity } from './AccountEntity';
+import { Column, Entity, Index, OneToMany, OneToOne } from "typeorm";
+import { AppEntity } from './AppEntity';
+
+@Index("client_cli_email_Idx", ["cliEmail"], { unique: true })
+@Index("pkclient", ["cliId"], { unique: true })
+@Index("client_cli_phone_Idx", ["cliPhone"], { unique: true })
+@Entity("client", { schema: "public" })
+export class ClientEntity {
+  @Column("uuid", { primary: true, name: "cli_id" })
+  cliId: string;
+
+  @Column("character varying", { name: "cli_full_name", length: 500 })
+  cliFullName: string;
+
+  @Column("character varying", { name: "cli_email", length: 500 })
+  cliEmail: string;
+
+  @Column("character varying", { name: "cli_phone", length: 500 })
+  cliPhone: string;
+
+  @Column("character varying", { name: "cli_photo", length: 500 })
+  cliPhoto: string;
+
+  @Column("integer", { name: "cli_state", default: () => "1" })
+  cliState: number;
+
+  @Column("timestamp without time zone", {
+    name: "cli_created_at",
+    default: () => "now()",
+  })
+  cliCreatedAt: Date;
+
+  @Column("timestamp without time zone", {
+    name: "cli_updated_at",
+    nullable: true,
+  })
+  cliUpdatedAt: Date | null;
+
+  @Column("timestamp without time zone", {
+    name: "cli_deleted_at",
+    nullable: true,
+  })
+  cliDeletedAt: Date | null;
+
+  @OneToOne(() => AccountEntity, (account) => account.cli)
+  account: AccountEntity;
+
+  @OneToOne(() => AppEntity, (app) => app.cli)
+  app: AppEntity;
+
+  @OneToMany(() => TokenEntity, (token) => token.cli)
+  tokens: TokenEntity[];
+}
