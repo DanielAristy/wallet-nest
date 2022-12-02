67ac4c141fdfba9f87d236e1eac7514ac0b38dc2
Daniel Aristizabal <daniel.aristizabal@sofka.com.co>
2022-12-02 14:40:30 -0500

    Se organiza en carpetas los diferentes componentes del aplicativo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,19 @@
-import { TokenEntity } from './entities/TokenEntity';
-import { MovementEntity } from './entities/MovementEntity';
-import { ClientEntity } from './entities/ClientEntity';
-import { AppEntity } from './entities/AppEntity';
-import { AccountEntity } from './entities/AccountEntity';
-import { AppService } from './app.service';
+import { TokenEntity } from './common/postgres/entities/TokenEntity';
+import { MovementEntity } from './common/postgres/entities/MovementEntity';
+import { ClientEntity } from './common/postgres/entities/ClientEntity';
+import { AppEntity } from './common/postgres/entities/AppEntity';
+import { AccountEntity } from './common/postgres/entities/AccountEntity';
+import { AppService } from './modules/main/service/app.service';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AppController } from './app.controller';
+import { AppController } from './modules/main/controller/app.controller';
+import { ClientModule } from './modules/client/client.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
+      host: '127.0.0.1',
       port: 5432,
       username: 'admin',
       password: 'admin',
@@ -20,6 +21,7 @@ import { AppController } from './app.controller';
       entities: [AccountEntity, AppEntity, ClientEntity, MovementEntity, TokenEntity],
       synchronize: false,
     }),
+    ClientModule,
   ],
   controllers: [AppController],
   providers: [AppService],
